c5fb645c5fd87de8685b665daa69997dabb63626
Guilherme Sitton de Carvalho <132395003+Gui-Sitton@users.noreply.github.com>
2024-11-13 09:05:25 -0300

    Delete assets directory

--- assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.carousel-next').forEach(button => {
-    button.addEventListener('click', () => {
-        const carousel = button.parentNode.querySelector('.carousel-inner');
-        carousel.scrollBy({ left: 300, behavior: 'smooth' });
-    });
-});
